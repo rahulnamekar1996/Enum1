e096a7980549e5d300c3b291fa64850e82c9cced
Owner <Owner@DESKTOP-2TPH2RF>
2023-07-12 19:20:11 +0530

    Add project files.

---
+++ Enum1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Student student=new Student(01,"rahul",Streams.mech);
+            Console.WriteLine(student);
+
+          Student s2 = new Student(02, "rohit", Streams.IT);
+            Console.WriteLine(s2);
+        }
+    }
+}
---
+++ Enum1/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum1
+{
+    public enum Streams { cs, IT,mech,electronics,civil }
+   
+
+    internal class Student
+    {
+        public int Id;
+        public string Name;
+        public int rollno;
+        public static int count;
+        private Streams stream;
+        public Student(int rollno, string name, Streams stream)
+        {
+            count++;
+            this.rollno = rollno;
+            this.Name = name;
+            this.Id = count;
+            this.stream = stream;
+
+        }
+        public static int GetCount()
+        {
+            return count;
+        }
+        public override string ToString()
+        {
+            return $"name is {Name} roll no{rollno} id is{Id} stream{stream}";
+        }
+
+    }
+}
